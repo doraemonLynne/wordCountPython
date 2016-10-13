86b3a85cc075c702ebf1e49dd6f1367a6b8460aa
Shi Linyong <linyong.shi@student.manchester.ac.uk>
2016-10-13 17:30:05 +0100

    forth submit

--- wc.py
+++ wc.py
@@ -1,120 +1,160 @@
-#!/usr/local/bin/python3.5
-# -*- coding:utf-8 -*-
+import sys
 
-import os,sys
-
-def fileLine(filepath):
-    thefile=open(filepath, 'rb')
-    linecount=len(thefile.readlines())
-    thefile.close()
+def fileLine(fileName):
+    try:
+        with open(fileName, 'r') as f:
+            linecount=len(f.readlines())
+    except IOError as err:
+        print ('wc:%s'%str(err))
+        exit()
     return linecount
 
-def fileWord(filepath):
-    thefile=open(filepath, 'rb')
-    wordcount=len(thefile.read().split())
-    thefile.close()
+def fileWord(fileName):
+    try:
+        with open(fileName, 'r') as f:
+            wordcount=len(f.read().split())
+    except IOError as err:
+        print ('wc:%s'%str(err))
+        exit()
     return wordcount
 
-def fileByte(filepath):
-    thefile=open(filepath, 'rb')
-    bytecount=len(thefile.read())
-    thefile.close()
+def fileByte(fileName):
+    try:
+        with open(fileName, 'r') as f:
+            bytecount=len(f.read())
+    except IOError as err:
+        print ('wc:%s'%str(err))
+        exit()
     return bytecount
+
+def stdinLine(lines):
+    linecount=len(lines)
+    return linecount
+
+def stdinWord(lines):
+    wordcount=0
+    for line in lines:
+        words=line.split(" ")
+        wordcount+=len(words)
+    return wordcount
+
+def stdinByte(lines):
+    bytecount=0
+    for line in lines:
+        bytecount+=len(line)
+    return bytecount
+
+def stdin():
+    sys.stdin = open('./testinputs/test4.txt', 'r')
+    sys.stdin.flush()
+    lines=sys.stdin.readlines()
+    linecount=len(lines)
+    wordcount=0
+    bytecount=0
+    for line in lines:
+        words=line.split(" ")
+        wordcount+=len(words)
+        bytecount+=len(line)
+    return linecount,wordcount,bytecount
 
 def fileHelp():
     print ('-- parameter information:')
     print ('    1. '+sys.argv[0]+ ' -l' +' filename ' +'line count')
     print ('    2. '+sys.argv[0]+ ' -w' +' filename ' +'word count')
-    print ('    3. '+sys.argv[0]+ ' -c' +' filename ' +'byte count')
-    print ('    4. '+sys.argv[0]+ '   ' +' filename ' +'all count')   
-    print ('    5. '+sys.argv[0]+ '   ' +' stdin input')
-    print ('    6. '+sys.argv[0]+ ' -help' +'  help information')
+    print ('    3. '+sys.argv[0]+ ' -c' +' filename ' +'byte count')   
+    print ('    4. '+sys.argv[0]+ '   ' +' stdin input')
+    print ('    5. '+sys.argv[0]+ ' -help' +'  help information')
 
-def getSpace(a,b,c):
-    space=max(len(str(a)),len(str(b)),len(str(c)))
-    return space
-
-if __name__ == '__main__':
-    if len(sys.argv)>=4 and (sys.argv[1]=='-l' or sys.argv[1]=='-w' or sys.argv[1]=='-c' or sys.argv[1]=='-help'):
-        if os.path.exists(sys.argv[2]) and os.path.exists(sys.argv[3]):
-            if sys.argv[1]=='-l' and os.path.isfile(sys.argv[3]):
-                print(str(fileLine(sys.argv[2])).center(8)+sys.argv[2])
-                print(str(fileLine(sys.argv[3])).center(8)+sys.argv[3])
-                print(str(fileLine(sys.argv[3])+fileLine(sys.argv[2])).center(7)+'total'.center(8))
-            elif sys.argv[1]=='-w' and os.path.isfile(sys.argv[3]):
-                print(str(fileWord(sys.argv[2])).center(8)+sys.argv[2])
-                print(str(fileWord(sys.argv[3])).center(8)+sys.argv[3])
-                print(str(fileWord(sys.argv[3])+fileWord(sys.argv[2])).center(7)+'total'.center(8))
-            elif sys.argv[1]=='-c' and os.path.isfile(sys.argv[3]):
-                print(str(fileByte(sys.argv[2])).center(8)+sys.argv[2])
-                print(str(fileByte(sys.argv[3])).center(8)+sys.argv[3])
-                print(str(fileByte(sys.argv[3])+fileByte(sys.argv[2])).center(7)+'total'.center(8))
-            elif sys.argv[1]=='-help' and os.path.isfile(sys.argv[2]):
+def getArgs():
+    flags=[]
+    files=[]   
+    for arg in sys.argv:
+        if arg[:1]=='-':
+            if arg=='-l' or arg=='-w' or arg=='-c':
+                if not arg in flags:
+                    flags.append(arg)
+            elif arg=='-help':
                 fileHelp()
-        elif os.path.exists(sys.argv[2])==False and os.path.exists(sys.argv[3])==False:
-            print ('-- file  '+sys.argv[2]+' and '+sys.argv[3]+'  does not exist'  )
-        elif os.path.exists(sys.argv[2])==False:
-            if sys.argv[1]=='-l' and os.path.isfile(sys.argv[3]):
-                print(str(fileLine(sys.argv[3])).center(8)+sys.argv[3])
-            elif sys.argv[1]=='-w' and os.path.isfile(sys.argv[3]):
-                print(str(fileWord(sys.argv[3])).center(8)+sys.argv[3])
-            elif sys.argv[1]=='-c' and os.path.isfile(sys.argv[3]):
-                print(str(fileByte(sys.argv[3])).center(8)+sys.argv[3])
-            print ('-- file  '+sys.argv[2]+'  does not exist'  )
-        elif os.path.exists(sys.argv[3])==False:
-            if sys.argv[1]=='-l' and os.path.isfile(sys.argv[2]):
-                print(str(fileLine(sys.argv[2])).center(8)+sys.argv[2])
-            elif sys.argv[1]=='-w' and os.path.isfile(sys.argv[2]):
-                print(str(fileWord(sys.argv[2])).center(8)+sys.argv[2])
-            elif sys.argv[1]=='-c' and os.path.isfile(sys.argv[2]):
-                print(str(fileByte(sys.argv[2])).center(8)+sys.argv[2])
-            print ('-- file  '+sys.argv[3]+'  does not exist'  )
-    elif len(sys.argv)>=3:
-        if sys.argv[1]=='-l' or sys.argv[1]=='-w' or sys.argv[1]=='-c' or sys.argv[1]=='-help':
-            if os.path.exists(sys.argv[2]):               
-                if sys.argv[1]=='-l' and os.path.isfile(sys.argv[2]):
-                    print(str(fileLine(sys.argv[2])).center(8)+sys.argv[2])
-                elif sys.argv[1]=='-w' and os.path.isfile(sys.argv[2]):
-                    print(str(fileWord(sys.argv[2])).center(8)+sys.argv[2])
-                elif sys.argv[1]=='-c' and os.path.isfile(sys.argv[2]):
-                    print(str(fileByte(sys.argv[2])).center(8)+sys.argv[2])
-                elif sys.argv[1]=='-help' and os.path.isfile(sys.argv[2]):
-                    fileHelp()
-            elif os.path.exists(sys.argv[2])==False:
-                print ('-- file  '+sys.argv[2]+'  does not exist'  )
-        elif os.path.isfile(sys.argv[1]):
-            if os.path.exists(sys.argv[1]) and os.path.exists(sys.argv[2]):
-                print(str(fileLine(sys.argv[1])).center(8)+str(fileWord(sys.argv[1])).center(8)+str(fileByte(sys.argv[1])).center(8)+sys.argv[1])
-                print(str(fileLine(sys.argv[2])).center(8)+str(fileWord(sys.argv[2])).center(8)+str(fileByte(sys.argv[2])).center(8)+sys.argv[2])
-                print(str(fileLine(sys.argv[1])+fileLine(sys.argv[2])).center(8)+str(fileWord(sys.argv[1])+fileWord(sys.argv[2])).center(8)+str(fileByte(sys.argv[1])+fileByte(sys.argv[2])).center(7)+'total'.center(8))
-            elif os.path.exists(sys.argv[2])==False:
-                print(str(fileLine(sys.argv[1])).center(8)+str(fileWord(sys.argv[1])).center(8)+str(fileByte(sys.argv[1])).center(8)+sys.argv[1])
-                print ('-- file  '+sys.argv[2]+'  does not exist'  )
-        elif os.path.exists(sys.argv[1])==False and os.path.exists(sys.argv[2])==False:
-            print ('-- file  '+sys.argv[1]+' and '+sys.argv[2]+'  does not exist'  )
-        elif os.path.exists(sys.argv[1])==False:
-            print(str(fileLine(sys.argv[2])).center(8)+str(fileWord(sys.argv[2])).center(8)+str(fileByte(sys.argv[2])).center(8)+sys.argv[2])
-            print ('-- file  '+sys.argv[1]+'  does not exist'  )
-    elif len(sys.argv)>=2:
-        if os.path.isfile(sys.argv[1]):
-            if os.path.exists(sys.argv[1]):
-                print(str(fileLine(sys.argv[1])).center(8)+str(fileWord(sys.argv[1])).center(8)+str(fileByte(sys.argv[1])).center(8)+sys.argv[1])
-        elif sys.argv[1]=='-help':
-            fileHelp()
-        elif os.path.exists(sys.argv[1])==False:
-            print ('-- file  '+sys.argv[1]+'  does not exist'  )
-    elif len(sys.argv)==1:
-        lines=sys.stdin.readlines()
-        words=0
-        bytess=0
-        for line in lines:
-            t=line.split(' ')
-            words+=len(t)
-            for eachline in t:
-                bytess+=len(eachline)
-        print (str(len(lines)).center(8)+str(words).center(8)+str(bytess).center(8))
+            else:
+                print("wc: illegal option -- '%s'"%arg[1:])
+        else:
+            if arg!='wc.py':
+                files.append(arg)
+
+    return {'flags':flags,'files':files}
+
+def getOutput(flags,line,word,byte,name=''):
+    output=''
+    if len(flags)!=0:
+        if not "-l" in flags:
+            line=''
+        if not "-w" in flags:
+            word=''
+        if not "-c" in flags:
+            byte=''
+    output=str(line).rjust(8)+str(word).rjust(8)+str(byte).rjust(8)+'  '+name
+    print (output)
+
+if __name__ == "__main__":
+    args=getArgs()
+    totalLine=0
+    totalWord=0
+    totalByte=0
+    line=0
+    word=0
+    byte=0
+    if len(args['files'])==0:
+        if len(args['flags'])==0:
+            lines=sys.stdin.readlines()
+            getOutput(args['flags'],stdinLine(lines),stdinWord(lines),stdinByte(lines))
+        else:
+            lines=sys.stdin.readlines()
+            for arg in args['flags']:
+                if arg=='-l':
+                    line=stdinLine(lines)
+                elif arg=='-w':
+                    word=stdinWord(lines)
+                elif arg=='-c':
+                    byte=stdinByte(lines)
+            getOutput(args['flags'],line,word,byte)
     else:
-        fileHelp()
-        
+        for file in args['files']:
+            if len(args['flags'])==0:
+                line=fileLine(file)
+                word=fileWord(file)
+                byte=fileByte(file)
+                totalLine+=line
+                totalWord+=word
+                totalByte+=byte
+            else:
+                for arg in args['flags']:
+                    if arg=='-l':
+                        line=fileLine(file)
+                        totalLine+=line
+                    if arg=='-w':
+                        word=fileWord(file)
+                        totalWord+=word
+                    if arg=='-c':
+                        byte=fileByte(file)
+                        totalByte+=byte
+            getOutput(args['flags'],line,word,byte,file)
+        if len(args['files'])>1:
+            getOutput (args['flags'],totalLine,totalWord,totalByte,"total")
+                        
+
+                
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+    
